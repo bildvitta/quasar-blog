fc4eeb772ded0c7f4f3dc929725ddb3a5f1b9e7e
felipejuzo02 <felipejuzo@hotmail.com>
2021-03-31 16:24:38 -0300

    structure of the authors and start in the process of dynamic addition

--- src/pages/authors/AuthorsCreate.vue
+++ src/pages/authors/AuthorsCreate.vue
@@ -1,6 +1,5 @@
 <template>
     <q-page class="bg-grey-2 q-pa-lg">
-      <div>
       <div class="flex justify-between">
         <div>
           <p class="text-h5 q-mb-xs">Criar Autor</p>
@@ -11,9 +10,54 @@
               <q-breadcrumbs-el label="Adicionar autor" />
             </q-breadcrumbs>
           </div>
-
         </div>
       </div>
+      <div class="q-my-lg relative-position">
+        <q-input outlined v-model="name" label="Nome do autor" class="q-my-md" />
+        <q-input outlined v-model="email" label="E-mail" />
+        <div class="q-my-lg">
+          <q-btn color="primary" label="Criar"  @click="addAuthor" />
+          <q-btn color="primary" flat label="Cancelar" />
+        </div>
+      </div>
+      <hr>
+       <div class="full-width">
+        <q-list bordered class="rounded-borders q-mb-xs">
+          <q-item clickable v-ripple v-for="(author, index) in authors" :key="index" class="flex row items-center" >
+            <p class="q-mb-none col">{{ author.name }}</p>
+            <p class="q-mb-none col">{{ author.email }}</p>
+            <q-btn icon="edit" unelevated @click="removeAuthor(index)" />
+          </q-item>
+        </q-list>
       </div>
     </q-page>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      authors: [],
+      name: '',
+      email: ''
+    }
+  },
+  methods: {
+    addAuthor () {
+      if (this.name === '' || this.email === '') {
+
+      } else {
+        this.authors.push({
+          name: this.name,
+          email: this.email
+        })
+        this.name = ''
+        this.email = ''
+      }
+    },
+    removeAuthor (index) {
+      this.authors.splice(index, 1)
+    }
+  }
+}
+</script>
--- src/pages/authors/AuthorsList.vue
+++ src/pages/authors/AuthorsList.vue
@@ -1,5 +1,5 @@
 <template>
-    <q-page class="bg-grey-2 q-pa-lg">
+    <q-page class="bg-grey-2 q-pa-lg page-authors-list">
       <div class="flex justify-between">
         <div>
           <p class="text-h5 q-mb-xs">Autores</p>
@@ -9,9 +9,32 @@
               <q-breadcrumbs-el label="Autores" />
             </q-breadcrumbs>
           </div>
-
         </div>
         <q-btn icon="add" unelevated rounded color="primary" label="Adicionar autor" :to="{ name: 'AuthorsCreate'}" />
       </div>
+      <div class="flex q-my-lg justify-between">
+        <q-input v-model="text" label="Procurar" class="page-posts-list__search col" />
+      </div>
+
     </q-page>
 </template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      text: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .page-authors-list{
+    &__image-author{
+      width: 30px;
+      border-radius: 50%;
+    }
+  }
+</style>
